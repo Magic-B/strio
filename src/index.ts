1f3f60cf3fb6c7ada3625d250e48176cdeaf2d8c
Magic-B <majorheby@gmail.com>
2025-11-29 18:21:29 +0300

    feat: consolidate exports from core modules

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+export { between, betweenAll } from "./core/between";
+export { capitalize, capitalizeWords } from "./core/capitalize";
+export { camelize, pascalize, dasherize, underscore } from "./core/case";
+export {
+  isEmpty,
+  isBlank,
+  isAlpha,
+  isNumeric,
+  isAlphaNumeric,
+  isEmail,
+  isUrl,
+} from "./core/checks";
+export { count } from "./core/count";
+export { escapeHtml, unescapeHtml, escapeRegExp } from "./core/escape";
+export { join } from "./core/join";
+export { template } from "./core/template";
+export { padLeft, padRight, padCenter } from "./core/pad";
+export { repeat } from "./core/repeat";
+export { reverse } from "./core/reverse";
+export { slugify } from "./core/slugify";
+export {
+  stripTags,
+  collapseWhitespace,
+  stripPrefix,
+  stripSuffix,
+} from "./core/strip";
+export { transliterate } from "./core/transliterate";
+export { trim } from "./core/trim";
+export { truncate, truncateWords } from "./core/truncate";
+export { words, wordCount } from "./core/words";
